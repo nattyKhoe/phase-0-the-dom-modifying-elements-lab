5aea087c59b5ebee212faa198c6ca2d56b1e588d
nattyKhoe <natasha.kusuma@outlook.com>
2022-06-05 11:41:04 +1000

    commit tree d932c6a68b1e1c2a1e074baa47705f528e4f5c82

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.getElementById("main");
+document.body.removeChild(main);
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.setAttribute ("id", "victory");
+newHeader.textContent ="Natasha is the champion";
